0f322944b290ae36df354831976542d56c5e998f
esther <esthc@outlook.es>
2022-04-11 01:45:31 +0200

    Prueba 2

--- src/components/nav-bar-container/nav-bar-container.jsx
+++ src/components/nav-bar-container/nav-bar-container.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import NavBarLogo from "../nav-bar-logo/nav-bar-logo";
 import NavBarMenu from "../nav-bar-menu/nav-bar-menu";
+import logo from "../../images/Logo_negro.png"
 import './css/principal-bar.css';
 
 const Container = () => {
     return (
         <header className="nav-bar-container">
-            <NavBarLogo clsName="nav-bar-logo" img={'https://www.finedininglovers.com/es/sites/g/files/xknfdk1706/files/styles/article_1200_800_fallback/public/2022-02/tipi%20di%20arance%20-%20Finedininglovers.jpg?itok=-v2tcDmu'} description={"Son naranjas"} />
+            <div className="nav-bar-brand">
+                <NavBarLogo clsName="nav-bar-logo" img={logo} description={"Son naranjas"} />
+                <h2>furniture.</h2>
+            </div>
             <ul className="nav-bar-menuConcept">
-            <NavBarMenu name={"Menu"} reference={"#"}/>
-            <NavBarMenu name={"Quien somos"} reference={"#"}/>
+            <NavBarMenu name={"Menu"} reference={"https://orteil.dashnet.org/cookieclicker/"}/>
+            <NavBarMenu name={"Quienes Somos"} reference={"#"}/>
             <NavBarMenu name={"Contacto"} reference={"#"}/>
             </ul>
         </header>
