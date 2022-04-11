7c4c6241154dfbdec52e7520fce8e14250142df5
esther <esthc@outlook.es>
2022-04-11 20:58:56 +0200

    Home terminada

--- src/components/home-content/home-content.jsx
+++ src/components/home-content/home-content.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
-import './css/home-content.css'
+import { Button } from 'primereact/button';
+import "primereact/resources/themes/luna-green/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+import './css/home-content.css';
 
 const HomeContainer = () => {
     return (
         <div className="home-content-container">
-            <h1>Hola</h1>
+            <div className="btn-see-catalog">
+                <Button label="Puedes ver nuestro catálogo" className="p-button-rounded" id="btn-catalog" />
+            </div>
         </div>
       );
 }
