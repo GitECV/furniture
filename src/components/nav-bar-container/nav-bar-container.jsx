d07165287ada3812c382d11a5e311a090e88c936
esther <esthc@outlook.es>
2022-04-13 17:50:34 +0200

    Menu de hamburguesa pt 1

--- src/components/nav-bar-container/nav-bar-container.jsx
+++ src/components/nav-bar-container/nav-bar-container.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import NavBarLogo from "../nav-bar-logo/nav-bar-logo";
 import NavBarMenu from "../nav-bar-menu/nav-bar-menu";
+import { Sling as Hamburger } from 'hamburger-react'
 import logo from "../../images/Logo_negro.png"
 import './css/principal-bar.css';
 
 const Container = props => {
+    const [isOpen, setOpen] = useState(false);
+
     return (
         <header className="nav-bar-container">
             <div className="nav-bar-brand">
@@ -11,9 +15,20 @@ const Container = props => {
                 <h1 id="logo-text">{props.text}</h1>
             </div>
             <ul className="nav-bar-menuConcept">
+            <div className="bigscreen-menu">
             <NavBarMenu name={"Home"} path={"/"} />
             <NavBarMenu name={"Catálogo"} path={"/catalogo"}/>
             <NavBarMenu name={"Contacta"} path={"/contacta"}/>
+            </div>
+            <Hamburger toggled={isOpen} toggle={setOpen} onToggle={toggled => {
+                if (toggled) {
+                   document.querySelector(".home-content-container").classList.add("hidden-visibility");
+                   document.querySelector(".nav-bar-container").insertAdjacentHTML("afterend", "<h1 class='hamburguer-background'>Hola</h1>");
+                } else {
+                    document.querySelector(".home-content-container").classList.remove("hidden-visibility");
+                    document.querySelector(".hamburguer-background").remove();
+                }
+            }} />
             </ul>
         </header>
       );
