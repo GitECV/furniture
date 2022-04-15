6e5a79faf6ce620b27610688172dcb0232f98856
esther <esthc@outlook.es>
2022-04-15 15:42:55 +0200

    Prueba tele

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,12 @@
 import HomeContainer from './components/home-content/home-content.jsx';
-import NavBarContainer from './components/nav-bar-container/nav-bar-container.jsx'
+import NavBarContainer from './components/nav-bar-container/nav-bar-container.jsx';
+import HamburguerBack from "./components/hamburguer-menu-background/hamburguer-menu-background.jsx";
 
 function Home() {
   return (<div className="App">
     <NavBarContainer text={"furniture."}/>
     <HomeContainer />
+    <HamburguerBack />
   </div>);
 }
 
---
+++ src/components/hamburguer-menu-background/hamburguer-menu-background.jsx
@@ -0,0 +1,9 @@
+const HamburguerBack = () => {
+    return (
+        <div className="hamburguer-menu-background">
+            <h2>Hola</h2>
+        </div>
+      );
+}
+
+export default HamburguerBack;
