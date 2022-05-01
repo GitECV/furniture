8917d59a51d127976b64fe6e0b3b3f7b7d957d19
esther <esthc@outlook.es>
2022-05-01 19:35:17 +0200

    Menu lateral añadido

--- src/components/3d-element-container/3d-element-container.jsx
+++ src/components/3d-element-container/3d-element-container.jsx
@@ -1,28 +1,49 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Canvas, useLoader, useThree } from '@react-three/fiber'
 import { OrbitControls, PerspectiveCamera, useGLTF } from "@react-three/drei";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { TextureLoader } from 'three/src/loaders/TextureLoader.js';
+import  MaterialIcon  from '../gui-element-selector/gui-element-selector.jsx';
 import { proxy, useSnapshot } from 'valtio';
 import bucket from '../../3d-elements/cajonera.glb';
 import { Suspense, useRef, useState } from "react";
 import { DoubleSide } from "three";
 
-const state = proxy({
-  current: null,
-  items: {
-    cajon: "#506DFF",
-    pomo: "#ffffff",
-    mesilla: "#ffffff",
+import './css/3d-element-container.css';
+
+export default class TreeDElementContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: false,
+      material: null
+    };
   }
-});
+
+  handleNext = () => {
+    this.setState({
+      value: true
+    });
+  };
+
+  render() {
+    return (
+        <div className='container3d-full-item'>
+      <div className="model-container">
+        <CatalogItem doClick={this.handleNext} />
+      </div>
+        <GUI renderElement={this.state.value} Material={"metal"} />
+      </div>
+    );
+  }
+}
 
 function Model({ ...props }) {
   const group = useRef()
   const { nodes, materials } = useGLTF(bucket);
   return (
     <group ref={group} {...props} dispose={null}>
-      <mesh onClick={handlePomoChange} geometry={nodes.pomo.geometry} material={materials.pomo_mat} position={[-0.04, 0, 0]} rotation={[Math.PI / 2, 0, 0]} scale={0.04} />
+      <mesh onClick={props.doClick} geometry={nodes.pomo.geometry} material={materials.pomo_mat} position={[-0.04, 0, 0]} rotation={[Math.PI / 2, 0, 0]} scale={0.04} />
       <mesh geometry={nodes.mesilla.geometry} material={materials.mesilla_mat} position={[-0.04, 0, 0]} rotation={[Math.PI / 2, 0, 0]} scale={0.04} />
       <mesh geometry={nodes.cajon.geometry} material={materials.cajon_mat} position={[-0.04, 0, 0]} rotation={[Math.PI / 2, 0, 0]} scale={0.04} />
     </group>
@@ -33,7 +54,7 @@ const CatalogItem = props => {
   return(
     <Canvas camera={{ position: [0, -40, 20] }} style={{height: "100vh"}}>
          <Suspense fallback={null}>
-         <OrbitControls maxDistance={4} minDistance={1} maxPolarAngle={Math.PI / 2} maxZoom={2} />
+         <OrbitControls maxDistance={1.5} minDistance={1} maxPolarAngle={Math.PI / 2} maxZoom={2} />
          <PerspectiveCamera
           aspect={1200 / 600}
           radius={(1200 + 600) / 4}
@@ -42,30 +63,16 @@ const CatalogItem = props => {
          makeDefault />
           <ambientLight intensity={0.5} color={'red'} />
           <spotLight position={[10, 15, 10]} angle={0.3} castShadow={true} intensity={1} />
-            <Model />
+            <Model doClick={props.doClick} />
          </Suspense>
     </Canvas>
  );
+  }
+
+const GUI = (props) => {
+  let elementGUI = null;
+  if (props.renderElement === true) {
+    elementGUI = <MaterialIcon MaterialType={props.Material} />
+  }
+  return elementGUI;
 }
-
-const FullItem = props => {
-  return (
-    <div className='container3d-full-item'>
-    <CatalogItem />
-    </div>
-  );
-}
-
-const handlePomoChange = () => {
-  console.log("hola");
-}
-
-const handleCajonChange = () => {
-  console.log("Cajón");
-}
-
-const GUIMenu = props => {
-
-}
-
-export default FullItem;
